84fab9ee6b43f2da2cdf5069c0cf61dc434c00a7
Natalia Sharkova <154504554+SSharkAttack@users.noreply.github.com>
2025-12-09 16:51:39 +0300

    Refactor to save DataFrame as XLSX and confirm save
    Updated the script to save the DataFrame as an XLSX file and added a print statement to confirm the save location.

--- get_etf_share_classes_quotes.py
+++ get_etf_share_classes_quotes.py
@@ -1,17 +1,14 @@
-   import requests
-import pandas as pd
+import requests
+import pandas as pd #for data frame
 import json
-import os
-import openpyxl
+import os #for Downloads path
+import openpyxl #for xlsx file
 
 url_emissions = 'https://ws.cbonds.info/services/json/get_etf_share_classes_quotes/?lang=eng'
 
 json_data_emissions = {
         "auth": {"login": "Test", "password": "Test"},
-        #"filter":[{"field":"isin","operator":"in","value":"LI1341568817;IE00B53SZB19;FR0010010827;AT0000497227;LI0399611693;"
-                                                          #"AT0000A2R0P1;LI0445024057;FR001400K3S7;AT0000817952;AT0000A3FW41;"
-                                                          #"LU2207278073;DE000A40A516;LU2327434507;FR0010510800;LI1403619219;"
-                                                          #"LU0344810915;AT0000A1Z049;LI0477123637"}],
+        #"filter":[{"field":"isin","operator":"in","value":"LI1341568817;IE00B53SZB19;FR0010010827;AT0000497227;LI0399611693}],
         #"fields":[{"field": "id"}, {"field": "cupon_sum"}, {"field": "cupon_rate"}, {"field": "coupon_num"}, {"field": "emission_isin_code"}],
         "sorting": [{"field": "date", "order": "desc"}]
     }
@@ -31,6 +28,7 @@ print(df)
 downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
 file_name = os.path.join(downloads_path, "cbonds_etf_quotes.xlsx")
 
-#To CSV file
+#To XLSX file
 df.to_excel(file_name, index=False)
+
 print(f"Data saved to {file_name}")
